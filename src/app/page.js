6b6692710fa943724d0001b306cbd7ae4b379d74
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-05-20 18:18:32 +0530

    djfgkbsnrglbd

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import { Component } from "@/components/component/component";
+import HomeHero from "@/components/elements/Home/HomeHero";
 
 export default function Home() {
   return (
     <main>
+      <HomeHero />
       <Component />
     </main>
   )
